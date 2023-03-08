d1e307df52dcdd5b8a2e0a06ea53e889742d38ba
John Eckert <johnteckert@gmail.com>
2023-03-07 22:51:02 -0500

    add StartDay button

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -6,8 +6,39 @@ import { API_KEY, BASE_ID } from "./ENV";
 
 const Landing = () => {
   const BASE = new Airtable({ apiKey: API_KEY }).base(BASE_ID);
-  const today = moment().format("dddd MMMM Do");
-  const handleStartDay = () => {};
+  const today = moment().format("YYYY-MM-DD");
+  const handleStartDay = () => {
+    TABLES.forEach((table: any) => {
+      const defaultFieldValues = {};
+      table?.fields.forEach((field: any) => {
+        // @ts-ignore
+        defaultFieldValues[field] = 0;
+      });
+      BASE(table.name).create(
+        [
+          {
+            fields: {
+              ...defaultFieldValues,
+              Date: today,
+            },
+          },
+        ],
+        // @ts-ignore
+        function (err, records) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          // @ts-ignore
+          records.forEach(function (record) {
+            console.log(record.getId());
+          });
+        }
+      );
+    });
+    window.location.reload();
+  };
+
   return (
     <div className="page">
       <h3>{today}</h3>
